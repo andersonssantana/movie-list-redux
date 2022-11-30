3c74638efaa3a9e2f83f70091c3052d1e359634a
Danilo Vieira <danilovieira@MacBook-Air-de-Danilo.local>
2022-11-30 17:03:45 -0300

    add movies data

--- src/data.js
+++ src/data.js
@@ -5,6 +5,7 @@ const movies = [
     translation: 'Um Sonho de Liberdade',
     year: '1994',
     poster: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg',
+    genre: 'Drama',
   },
   {
     id: 262,
@@ -12,6 +13,7 @@ const movies = [
     translation: 'O Poderoso Chefão',
     year: '1972',
     poster: 'https://m.media-amazon.com/images/M/MV5BZjgwNzE5ODgtYzAyZC00YTZhLThhNDktMDVlOGNhYzk5NTVkXkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg',
+    genre: 'Criminal',
   },
   {
     id: 263,
@@ -19,6 +21,7 @@ const movies = [
     translation: 'Batman: O Cavaleiro das Trevas',
     year: '2008',
     poster: 'https://m.media-amazon.com/images/M/MV5BM2E1ZjEyYWQtMjgyMy00ZTkwLThkM2QtN2YwM2NmYzAyYjE0XkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_FMjpg_UX800_.jpg',
+    genre: 'Action',
   },
   {
     id: 264,
@@ -26,6 +29,7 @@ const movies = [
     translation: 'O Poderoso Chefão: Parte II',
     year: '1974',
     poster: 'https://m.media-amazon.com/images/M/MV5BOGM2M2Q0ZWQtZjc5NC00NDY4LWIxYTktMDE4ZjIyYTRlNTIyXkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_FMjpg_UX844_.jpg',
+    genre: 'Criminal',
   },
   {
     id: 265,
@@ -33,6 +37,7 @@ const movies = [
     translation: '12 Homens e uma Sentença',
     year: '1957',
     poster: 'https://m.media-amazon.com/images/M/MV5BODRmNWRiYmUtNGM2Yy00NTgwLTgxNzMtMGExYzI5NjllNjBhXkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg',
+    genre: 'Drama',
   },
   {
     id: 266,
@@ -40,6 +45,7 @@ const movies = [
     translation: 'A Lista de Schindler',
     year: '1993',
     poster: 'https://m.media-amazon.com/images/M/MV5BYjM5ZjYxNjQtZjdhYi00NzBiLTgyNGItOGU3YTYwYWE0NmEzXkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_FMjpg_UX675_.jpg',
+    genre: 'War',
   },
   {
     id: 267,
@@ -48,6 +54,14 @@ const movies = [
     year: '2020',
     genre: 'Fantasy',
     poster: 'https://m.media-amazon.com/images/M/MV5BYWFjNGQxZjItOTgxNy00NjhkLWFkZGQtOWVjNzJlYWNhM2NmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
+  },
+  {
+    id: 268,
+    title: 'The Lord of the Rings: The Return of the King',
+    translation: 'O Senhor dos Anéis: O Retorno do Rei',
+    year: '2003',
+    genre: 'Action',
+    poster: 'https://m.media-amazon.com/images/M/MV5BNzZiOTI4MWItZGMxZC00NjZkLWJlOWUtMDY5YzE1NzhiNGRhXkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_.jpg',
   },
 ];
 
